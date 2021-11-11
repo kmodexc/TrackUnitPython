9052e24cafeeea1996365b7eb5de993a677f6e39
Marius Schlueter <m.schlueter01@stud.uni-goettingen.de>
2021-11-11 16:30:13 +0100

    changes and try 5

--- pytrackunit/trackunit.py
+++ pytrackunit/trackunit.py
@@ -1,10 +1,9 @@
 """module TrackUnit"""
 
+from math import ceil
+from multiprocessing import Pool
 from datetime import timedelta,datetime
 from . import tucache
-from math import ceil
-from threading import Thread
-from multiprocessing import Pool
 
 class TrackUnit:
     """TrackUnit class"""
@@ -25,16 +24,17 @@ class TrackUnit:
         if self.verbose:
             print(req+"\t"+str(len(data)))
         return data
-    def get_unitlist(self,type=None,sortByHours=True):
+    def get_unitlist(self,_type=None,sort_by_hours=True):
         """unitList method"""
         data = self.get('Unit')
-        if type is not None:
-            data = list(filter(lambda x: " " in x['name'] and type in x['name'],data))
-        if sortByHours:
+        if _type is not None:
+            data = list(filter(lambda x: " " in x['name'] and _type in x['name'],data))
+        if sort_by_hours:
             data.sort(key=lambda x: (x['run1'] if 'run1' in x else 0),reverse=True)
         return data
-    def get_history(self,vehId,tdelta=None,start=None,end=None,threads=1):
+    def get_history(self,veh_id,tdelta=None,start=None,end=None,threads=1):
         """getHistory method"""
+        total_data = []
         if tdelta is None and start is not None and end is not None:
             days = (end-start).days
             requests = []
@@ -44,18 +44,16 @@ class TrackUnit:
                 wstartstr = wstart.strftime("%Y-%m-%dT%H:%M:%S")
                 wend = wstart+timedelta(days=min(request_period,(end-wstart).days))
                 wendstr = wend.strftime("%Y-%m-%dT%H:%M:%S")
-                req = 'Report/UnitHistory?unitId='+vehId+'&from='+wstartstr+\
+                req = 'Report/UnitHistory?unitId='+veh_id+'&from='+wstartstr+\
                     '.0000001Z&to='+wendstr+'.0000000Z'
                 requests.append(req)
             if threads > 1:
-                with Pool(threads) as p:
-                    map_result = p.map(self.get,requests)
+                with Pool(threads) as _p:
+                    map_result = _p.map(self.get,requests)
             else:
                 map_result = map(self.get,requests)
-            totalData = []
-            for r in map_result:
-                totalData += r
-            return totalData
+            for _r in map_result:
+                total_data += _r
         elif tdelta is not None and start is None and end is None:
             end = datetime.now()
             end = end.replace(hour=0,minute=0,second=0,microsecond=0)
@@ -66,11 +64,13 @@ class TrackUnit:
                 if irange <= 0:
                     return []
                 start = end-timedelta(days=irange)
-            return self.get_history(vehId,None,start,end,threads)
+            total_data = self.get_history(veh_id,None,start,end,threads)
         else:
             raise Exception("invalid argument combination of tdelta,start,end")
-    def get_candata(self,vehId,tdelta=None,start=None,end=None,threads=1):
+        return total_data
+    def get_candata(self,veh_id,tdelta=None,start=None,end=None,threads=1):
         """getCanData method"""
+        total_data = []
         if tdelta is None and start is not None and end is not None:
             days = (end-start).days
             requests = []
@@ -80,18 +80,16 @@ class TrackUnit:
                 wstartstr = wstart.strftime("%Y-%m-%dT%H:%M:%S")
                 wend = wstart+timedelta(days=min(request_period,(end-wstart).days))
                 wendstr = wend.strftime("%Y-%m-%dT%H:%M:%S")
-                req = 'Report/UnitExtendedInfo?Id='+vehId+'&from='+wstartstr+\
+                req = 'Report/UnitExtendedInfo?Id='+veh_id+'&from='+wstartstr+\
                     '.0000001Z&to='+wendstr+'.0000000Z'
                 requests.append(req)
             if threads > 1:
-                with Pool(threads) as p:
-                    map_result = p.map(self.get,requests)
+                with Pool(threads) as _p:
+                    map_result = _p.map(self.get,requests)
             else:
                 map_result = map(self.get,requests)
-            totalData = []
-            for r in map_result:
-                totalData += r
-            return totalData
+            for _r in map_result:
+                total_data += _r
         elif tdelta is not None and start is None and end is None:
             end = datetime.now()
             end = end.replace(hour=0,minute=0,second=0,microsecond=0)
@@ -102,6 +100,7 @@ class TrackUnit:
                 if irange <= 0:
                     return []
                 start = end-timedelta(days=irange)
-            return self.get_candata(vehId,None,start,end,threads)
+            total_data = self.get_candata(veh_id,None,start,end,threads)
         else:
             raise Exception("invalid argument combination of tdelta,start,end")
+        return total_data
