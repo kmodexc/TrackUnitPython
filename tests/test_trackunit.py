96965a220d7cdf4aa238026639e6f1e2740d89ca
Marius Schlueter <m.schlueter01@stud.uni-goettingen.de>
2021-11-15 23:52:17 +0100

    added check url test

--- tests/test_trackunit.py
+++ tests/test_trackunit.py
@@ -1,6 +1,21 @@
+from datetime import datetime
 from pytrackunit.trackunit import TrackUnit
-import os
-import shutil
+
+BASE_URL = "https://api.trackunit.com/public/Report/UnitExtendedInfo?Id=3331359"
+
+class CacheForTests:
+    """tucache class"""
+    def __init__(self):
+        self.urls = []
+    def clean(self):
+        pass
+    def get(self,url):
+        print(url)
+        self.urls.append(url)
+        retval = {}
+        retval["list"] = []
+        return retval
+
 def test_get():
 	tu = TrackUnit()
 	tu.cache.dir = "pytest-web-cache"
@@ -39,3 +54,14 @@ def test_getcandata_no_file_read():
 	data = tu.get_candata("3331359",tdelta=100)
 	assert len(data) == 0
 	assert data == []
+def test_getcandata_check_urls():
+	tu = TrackUnit()
+	tu.tdelta_end = datetime(2021,11,15,10,0,0,0)
+	tu.cache = CacheForTests()
+	tu.get_candata("3331359",tdelta=100)
+	assert len(tu.cache.urls) == 4
+	assert tu.cache.urls[0] == BASE_URL+"&from=2021-08-07T00:00:00.0000001Z&to=2021-09-06T00:00:00.0000000Z"
+	assert tu.cache.urls[1] == BASE_URL+"&from=2021-09-06T00:00:00.0000001Z&to=2021-10-06T00:00:00.0000000Z"
+	assert tu.cache.urls[2] == BASE_URL+"&from=2021-10-06T00:00:00.0000001Z&to=2021-11-05T00:00:00.0000000Z"
+	assert tu.cache.urls[3] == BASE_URL+"&from=2021-11-05T00:00:00.0000001Z&to=2021-11-15T00:00:00.0000000Z"
+	
