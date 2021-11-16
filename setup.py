cc5850a997c44362c07e0e15312d1ad76fc7221f
Marius Schlueter <m.schlueter01@stud.uni-goettingen.de>
2021-11-16 13:36:09 +0100

    v1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pytrackunit",
-    version="1.2.0",
+    version="1.2.1",
     author="Marius Schlueter",
     author_email="themrslue@googlemail.com",
     description="Easy access for TrackUnit REST API",
