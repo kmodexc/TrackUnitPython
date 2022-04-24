44f8ec3b78135e5c4a478b59bd1236bc2b8fc21f
Marius Schlüter <themrslue@googlemail.com>
2022-04-24 20:19:00 +0200

    use verbose in tests

--- tests/test_real_data.py
+++ tests/test_real_data.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from pytrackunit.trackunit import TrackUnit
 
 def test_getunitlist():
-	tu = TrackUnit()
+	tu = TrackUnit(verbose=True)
 	#tu.cache.dir = "pytest-web-cache"
 	data = tu.get_unitlist("WNK41636VKTKF0002")
 	assert len(data) == 1
@@ -10,25 +10,25 @@ def test_getunitlist():
 	assert data[0]["name"] == "KT559-36 (416-36) WNK41636VKTKF0002"
 	tu.cache.clean()
 def test_gethistory():
-	tu = TrackUnit()
+	tu = TrackUnit(verbose=True)
 	#tu.cache.dir = "pytest-web-cache"
 	data = tu.get_history("3331359",tdelta=100)
 	assert len(data) > 1000
 	tu.cache.clean()
 def test_getcandata():
-	tu = TrackUnit()
+	tu = TrackUnit(verbose=True)
 	#tu.cache.dir = "pytest-web-cache"
 	data = tu.get_candata("3331359",tdelta=100)
 	assert len(data) > 10000
 	tu.cache.clean()
 def test_getfaults():
-	tu = TrackUnit()
+	tu = TrackUnit(verbose=True)
 	#tu.cache.dir = "pytest-web-cache"
 	data = tu.get_faults("3331359",tdelta=100)
 	assert len(data) > 10
 	tu.cache.clean()
 def test_getcandata_no_file_read():
-	tu = TrackUnit()
+	tu = TrackUnit(verbose=True)
 	tu.cache.cache.dont_read_files = True
 	#tu.cache.dir = "pytest-web-cache"
 	data = tu.get_candata("3331359",tdelta=100)
