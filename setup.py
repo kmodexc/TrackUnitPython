8473c961477da92a34666e35345659296d855418
Marius Schlüter <themrslue@googlemail.com>
2022-04-24 20:36:38 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pytrackunit",
-    version="2.3.4",
+    version="2.3.5",
     author="Marius Schlueter",
     author_email="themrslue@googlemail.com",
     description="Easy access for TrackUnit REST API",
