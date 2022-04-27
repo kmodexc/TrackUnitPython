a426253c8715caad7b5622b3add106b898565bb1
Marius Schlüter <themrslue@googlemail.com>
2022-04-27 17:25:34 +0200

    removed unused no file read test; added sqlcache tests

--- tests/test_real_data.py
+++ tests/test_real_data.py
@@ -1,40 +1,53 @@
-from datetime import datetime
+import os
 from pytrackunit.trackunit import TrackUnit
 
+DB_PATH = "pytest_db.sqlite"
+
 def test_getunitlist():
-	tu = TrackUnit(verbose=True)
-	#tu.cache.dir = "pytest-web-cache"
-	data = tu.get_unitlist("WNK41636VKTKF0002")
-	assert len(data) == 1
-	assert data[0]["serialNumber"] == "3603666"
-	assert data[0]["name"] == "KT559-36 (416-36) WNK41636VKTKF0002"
-	tu.cache.clean()
+    tu = TrackUnit(verbose=True)
+    #tu.cache.dir = "pytest-web-cache"
+    data = tu.get_unitlist("WNK41636VKTKF0002")
+    assert len(data) == 1
+    assert data[0]["serialNumber"] == "3603666"
+    assert data[0]["name"] == "KT559-36 (416-36) WNK41636VKTKF0002"
+    tu.cache.clean()
 def test_gethistory():
-	tu = TrackUnit(verbose=True)
-	#tu.cache.dir = "pytest-web-cache"
-	data = tu.get_history("3331359",tdelta=100)
-	assert len(data) > 1000
-	tu.cache.clean()
+    tu = TrackUnit(verbose=True)
+    #tu.cache.dir = "pytest-web-cache"
+    data = tu.get_history("3331359",tdelta=100)
+    assert len(data) > 1000
+    tu.cache.clean()
 def test_getcandata():
-	tu = TrackUnit(verbose=True)
-	#tu.cache.dir = "pytest-web-cache"
-	data = tu.get_candata("3331359",tdelta=100)
-	assert len(data) > 10000
-	tu.cache.clean()
+    tu = TrackUnit(verbose=True)
+    #tu.cache.dir = "pytest-web-cache"
+    data = tu.get_candata("3331359",tdelta=100)
+    assert len(data) > 10000
+    tu.cache.clean()
 def test_getfaults():
-	tu = TrackUnit(verbose=True)
-	#tu.cache.dir = "pytest-web-cache"
-	data = tu.get_faults("3331359",tdelta=100)
-	assert len(data) > 10
-	tu.cache.clean()
-def test_getcandata_no_file_read():
-	tu = TrackUnit(verbose=True)
-	tu.cache.cache.dont_read_files = True
-	#tu.cache.dir = "pytest-web-cache"
-	data = tu.get_candata("3331359",tdelta=100)
-	assert len(data) > 10000
-	data = tu.get_candata("3331359",tdelta=100)
-	assert len(data) == 0
-	assert data == []
-	tu.cache.clean()
-	
+    tu = TrackUnit(verbose=True)
+    #tu.cache.dir = "pytest-web-cache"
+    data = tu.get_faults("3331359",tdelta=100)
+    assert len(data) > 10
+    tu.cache.clean()
+def test_gethistory_sqlcache():
+    if os.path.isfile(DB_PATH):
+        os.remove(DB_PATH)
+    tu = TrackUnit(db_path=DB_PATH,verbose=True,tu_use_sqlcache=True)
+    #tu.cache.dir = "pytest-web-cache"
+    data = tu.get_history("3331359",tdelta=100)
+    assert len(data) > 1000
+def test_getcandata_sqlcache():
+    if os.path.isfile(DB_PATH):
+        os.remove(DB_PATH)
+    tu = TrackUnit(db_path=DB_PATH,verbose=True,tu_use_sqlcache=True)
+    #tu.cache.dir = "pytest-web-cache"
+    data = tu.get_candata("3331359",tdelta=100)
+    assert len(data) > 10000
+def test_getfaults_sqlcache():
+    if os.path.isfile(DB_PATH):
+        os.remove(DB_PATH)
+    tu = TrackUnit(db_path=DB_PATH,verbose=True,tu_use_sqlcache=True)
+    #tu.cache.dir = "pytest-web-cache"
+    data = tu.get_faults("3331359",tdelta=100)
+    assert len(data) > 10
+    
